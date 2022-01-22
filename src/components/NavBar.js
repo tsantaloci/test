cad7f99d096cd035d81db92234a619ec18bfa08e
tsantaloci <tsantalo@go.olemiss.edu>
2022-01-22 15:40:53 -0600

    website

---
+++ src/components/NavBar.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import CustomBtn from './CustomBtn'
+import logo from '../JCS.png'
+import logoMobile from '../logoMobile.svg'
+import {Toolbar,Typography} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+
+const styles = makeStyles({
+    bar:{
+        paddingTop: "1.15rem",
+        backgroundColor:"#fff",
+        ['@media (max-width:780px)']:{
+            flexDirection: "column"
+        }
+    },
+    logo: {
+        width: "15%",
+        ['@media (max-width:780px)']:{
+            display: "none"
+        }
+
+
+    },
+    logoMobile:{
+        width: "100%",
+        display: "none",
+        ['@media (max-width:780px)']:{
+            display: "inline-block"
+        }
+    },
+    menuItem:{
+        cursor: "pointer",
+        flexGrow:1,
+        "&:hover":{
+            color: "#4f25c8"
+        },
+        ['@media (max-width:780px)']:
+        {paddingBottom:"1rem"}
+        }
+})
+
+function NavBar() {
+    const classes = styles()
+    return (
+        <Toolbar position = "sticky" color = "rgba(0,0,0,0.87)" className={classes.bar}>
+            <img src={logo} className={classes.logo} />
+            <img src={logoMobile} className={classes.logoMobile} />
+            <Typography variant="h6" className={classes.menuItem}>
+                Home
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Trailor Inventory
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Metal Fab Inventory
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Trailor Services
+            </Typography>
+            <Typography variant="h6" className={classes.menuItem}>
+                Contact Us
+            </Typography>
+            <CustomBtn txt="(###) ###-#### Toll Free" />
+        </Toolbar>
+
+    )
+}
+
+export default NavBar
